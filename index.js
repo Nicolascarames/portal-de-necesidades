f001e367fd1330f6b6c20ecdbdb93fefbba7130d
Matteo <matteo.stella.web@gmail.com>
2023-05-07 15:12:42 +0200

    Final setup base app

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const BDconn = require('./db');
 const app = express();
+const router = require('./src/routes/index')
 const env = require('dotenv').config();
 app.use(cors());
 app.listen(4000);
 app.use(express.json());
+app.use(router)
 
-app.get('/users', async (req, res) => {
-  const connect = await BDconn();
-  const [response] = await connect.query('DESCRIBE users ');
-  console.log(response);
-  res.send('recibido!');
-});
 
 console.log('hello world');
