22674a4fa6d6eb2fbb6c77c5bde08dcbf9f71dbd
Matteo <matteo.stella.web@gmail.com>
2023-05-07 12:56:13 +0200

    default changes

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const express = require('express')
+const cors = require('cors');
+const {BDconn} = require('./db');
+const app= express();
+app.use(cors());
+app.listen(4000);
+app.use(express.json())
+
+
+
+app.get('/add',async (req,res)=>{
+   const connect = await BDconn();
+   const [response]= await connect.query('CREATE TABLE users(id INT PRIMARY KEY) ');
+   console.log(response)
+    res.send('recibido!')
+})
+
 
 console.log('hello world')
