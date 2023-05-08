e2be3b5626f92663d085365908981cd8a008cafc
nico-home-windows <caramesgarcianico@gmail.com>
2023-05-08 18:04:57 +0200

    borrado test

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,16 +1,3 @@
-// const BDconn = require('../database/db');
-
-//   const Test = async(req, res) => {
-//     const connect = await BDconn();
-//     const [response] = await connect.query('DESCRIBE users ');
-//     console.log(response);
-//     res.send('recibido!');
-//   }
-//   const Prueba = (req,res) =>{
-//     res.send('recibido pueba2!')
-//   }
-//   module.exports = {Test,Prueba}
-
 const loginUser = require('./login');
 const getUser = require('./getUser');
 
