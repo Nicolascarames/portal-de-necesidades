413cbe7fcce53afab7ca132edcdd75e17f41a748
Matteo <matteo.stella.web@gmail.com>
2023-05-08 18:42:37 +0200

    hay que areglar

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const Upload = require('../middleware/ImageUpload')
 const router = express.Router();
-const { loginUser, getUser ,AddService} = require('../controllers');
+//const  loginUser  = require('../controllers/login');
+const getUser = require('../controllers/getUser')
+ const { AddService } = require('../controllers')
 
 
 
-
+//router.post('/login', loginUser);
+router.get('/getuser/:id', getUser);
 
 
 router.post('/addservice',Upload,AddService)
-module.exports = router; 
+module.exports = router;  
